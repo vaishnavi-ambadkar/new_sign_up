376d38b9b6b00152b50d5ae53ec9fbf26bd50202
vaishnavi-ambadkar <vaishnaviambadkar183@gmail.com>
2025-01-17 19:47:09 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(express.json())
 app.use(cors(
   {
-  origin:["https://new-sign-up-frontend.vercel.app/"],
+  origin:["https://new-sign-up-frontend.vercel.app"],
   methods:["POST","GET"],
   credentials:ture
   }
